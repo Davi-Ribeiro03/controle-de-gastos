2880da789f491c98d7e7c667d9208709087b8ccc
Davi-OSF <davi.ribeiro@osf.digital>
2026-01-07 17:14:38 -0300

    adds Reactotron

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Router from "./src/Router/Router";
 
+if (__DEV__) {
+  require("./ReactotronConfig");
+}
+
 export default function App() {
   return <Router />;
 }
